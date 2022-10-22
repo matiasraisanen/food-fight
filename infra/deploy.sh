77150105c0652b19fcd6d9490c89b394b8b2ddda
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-22 23:04:24 +0000

    update script

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -1,1 +1,20 @@
-npx cdk deploy --profile myPersonalAws --require-approval never --all 
+# #!/bin/bash
+# # I like colours on my scripts
+# D='\033[1;30m' # Dark Grey
+# B='\033[0;35m' #
+# O='\033[0;34m' # Orange
+# R='\033[0;31m' # Red
+# Y='\033[0;33m' # Yellow
+# G='\033[0;32m' # Green
+# N='\033[0m'    # No Color
+
+# printf "\n"
+# printf "${G}* MANAGEMENT SCRIPT FOR KOODIHAASTE APPLICATION *\n"
+# printf "\n"
+
+# If you are not me, you should change this.
+# More @ https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+AWS_PROFILE_NAME="myPersonalAws"
+
+
+npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all 
