5f384b95dd14c75801c3ad4ea68e9186bde6c56e
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-22 09:38:36 +0000

    rename stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -4,7 +4,6 @@ import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
 const app = new cdk.App();
-new InfraStack(app, "InfraStack", {
-  env: { account: "998999048443", region: "eu-north-1" },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new InfraStack(app, "KoodihaasteInfraStack", {
+  env: { account: "998999048443", region: "eu-west-1" },
 });
