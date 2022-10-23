b0756c95901768fdeb9126bd9ec063a7e384d3f6
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-23 20:31:48 +0000

    front updates

--- static/src/App.js
+++ static/src/App.js
@@ -1,25 +1,63 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import FighterCard from './components/FighterCard.js';
-import FightLog from './components/FightLog';
-import { Button } from 'react-bootstrap';
-
-import SearchForm from './components/SearchForm.js';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FighterCard from "./components/FighterCard.js";
+import FightLog from "./components/FightLog";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 // import { Card, Table } from 'react-bootstrap';
 
 function App() {
+  const [player1, setPlayer1] = useState({
+    name: "-",
+    energy: "",
+    carbs: "",
+    protein: "",
+    fat: "",
+    cooldown: "",
+  });
+  const [player2, setPlayer2] = useState({
+    name: "-",
+    energy: "",
+    carbs: "",
+    protein: "",
+    fat: "",
+    cooldown: "",
+  });
+
   return (
     <div className="App">
-      <div className="Title"><h1>FOOD FIGHT</h1></div>
-
-      <div className="App-cardZone">
-        <FighterCard name="omena" realName="Omena, ulkomainen, kuorineen" energy="38.852571462547175" carbs="8.19540006637573" protein="0.165299997925758 " fat="100" cooldown="8.447700065597887" image="./images/p1.png" />
-        <FighterCard name="makkara" realName="Raakamakkara, siskonmakkara" energy="212.05504207491708" carbs="0.907999961197376" protein="10.0061154522777" fat="16.7246096517605" cooldown="27.638725065235576" player="2" image="./images/p2.png" />
+      <div className="Title">
+        <h1>FOOD FIGHT</h1>
       </div>
 
+      <div className="App-cardZone">
+
+        <FighterCard
+          name={player1.name}
+          energy={player1.energy}
+          carbs={player1.carbs}
+          protein={player1.protein}
+          fat={player1.fat}
+          cooldown={player1.cooldown}
+          playerNo="1"
+        />
+
+        <FighterCard
+          name={player2.name}
+          energy={player2.energy}
+          carbs={player2.carbs}
+          protein={player2.protein}
+          fat={player2.fat}
+          cooldown={player2.cooldown}
+          playerNo="2"
+        />
+
+      </div>
 
       <div>
-        <Button variant="danger" size="lg" style={{ color: "black", "font-weight": "bold" }}>FIGHT</Button>
+        <Button variant="danger" size="lg" style={{ color: "black", "font-weight": "bold" }}>
+          FIGHT
+        </Button>
       </div>
 
       <div>
--- static/src/components/FighterCard.js
+++ static/src/components/FighterCard.js
@@ -1,41 +1,69 @@
-import { Card, Table, Button } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
+import React, { useState, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-export default function FighterCard({ name, realName, energy, carbs, protein, fat, cooldown, image }) {
+export default function FighterCard({ name, energy, carbs, protein, fat, cooldown, playerNo }) {
+  const [show, setShow] = useState(false);
+  const [target, setTarget] = useState(null);
+  const ref = useRef(null);
+
+  const handleClick = (event) => {
+    setShow(!show);
+    setTarget(event.target);
+  };
+
+  const player = {
+    name: name,
+    energy: energy === "" ? 0 : parseInt(energy),
+    carbs: carbs === "" ? 0 : parseInt(carbs).toPrecision(2),
+    protein: protein === "" ? 0 : `${parseInt(protein).toPrecision(3)}%`,
+    fat: fat === "" ? 0 : parseInt(fat),
+    cooldown: cooldown === "" ? 0 : parseInt(cooldown),
+    speed: cooldown === "" ? 0 : parseFloat(1 / cooldown).toPrecision(2),
+    dps: carbs === "" ? 0 : (carbs / cooldown).toFixed(3)
+  }
 
   return (
-    <div style={{ padding: '5vh' }}>
+    <div ref={ref} style={{ padding: '5vh' }}>
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={image} />
+        <Card.Img variant="top" src={`./images/p${playerNo}.png`} />
         <Card.Header as="h5">{name.toUpperCase()}</Card.Header>
         <Card.Body>
           <Table striped bordered hover responsive>
             <tbody>
               <tr>
                 <td>HP</td>
-                <td>{parseInt(energy)}</td>
+                <td>{player.energy}</td>
               </tr>
               <tr>
                 <td>DMG</td>
-                <td>{parseFloat(carbs).toPrecision(2)}</td>
+                <td>{player.carbs}</td>
               </tr>
               <tr>
                 <td>DEF</td>
-                <td>{parseFloat(protein).toPrecision(3)}%</td>
+                <td>{player.protein}</td>
               </tr>
               <tr>
                 <td>SPEED</td>
-                <td>{parseFloat(1 / cooldown).toPrecision(2)}</td>
+                <td>{player.speed}</td>
               </tr>
               <tr>
                 <td>DPS</td>
-                <td>{(carbs / cooldown).toFixed(3)}</td>
+                <td>{player.dps}</td>
               </tr>
             </tbody>
           </Table>
 
-          <Button variant="primary" type="submit" className="mb-2">
+          <Button onClick={handleClick} variant="secondary" type="submit" className="mb-2">
             CHANGE FIGHTER
           </Button>
+
+
         </Card.Body>
       </Card>
     </div>
