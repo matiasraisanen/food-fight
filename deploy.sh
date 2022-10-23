e3a7c37f6c4ffba2ac712e1f1fe79c9f4ad33cf9
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-23 12:06:33 +0000

    update script

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,22 @@
 # O='\033[0;34m' # Orange
 # R='\033[0;31m' # Red
 # Y='\033[0;33m' # Yellow
-# G='\033[0;32m' # Green
-# N='\033[0m'    # No Color
+G='\033[0;32m' # Green
+N='\033[0m'    # No Color
 
-# printf "\n"
-# printf "${G}* MANAGEMENT SCRIPT FOR KOODIHAASTE APPLICATION *\n"
-# printf "\n"
+printf "\n"
+printf "${G}* MANAGEMENT SCRIPT FOR KOODIHAASTE APPLICATION *${N}\n"
+printf "\n"
 
 # If you are not me, you should change this.
 # More @ https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 AWS_PROFILE_NAME="myPersonalAws"
 
-
+# Build static website
 cd static
 npm run build
 cd ..
 
+# Deploy application to AWS
 cd infra
 npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all 
