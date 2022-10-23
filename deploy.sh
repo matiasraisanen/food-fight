15da7d17cc85046c169bbd754d20295a1155c149
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-23 21:01:18 +0000

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 # #!/bin/bash
-# # I like colours on my scripts
-# D='\033[1;30m' # Dark Grey
-# B='\033[0;35m' #
-# O='\033[0;34m' # Orange
+# # I like colours on my scripts :-)
+
 # R='\033[0;31m' # Red
 # Y='\033[0;33m' # Yellow
 G='\033[0;32m' # Green
@@ -12,7 +10,7 @@ printf "\n"
 printf "${G}* MANAGEMENT SCRIPT FOR KOODIHAASTE APPLICATION *${N}\n"
 printf "\n"
 
-# If you are not me, you should change this.
+# If you are not me, you should change this profile.
 # More @ https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 AWS_PROFILE_NAME="myPersonalAws"
 
