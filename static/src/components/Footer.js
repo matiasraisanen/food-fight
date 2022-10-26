e8dcfb229e0542e5a4b7372bda0c13f6f229d9ab
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-26 19:08:34 +0000

    add footer

--- static/src/components/Footer.js
+++ static/src/components/Footer.js
@@ -1,19 +1,11 @@
-import React, { Component } from "react";
-var year = new Date().getFullYear();
-
-class Footer extends Component {
-  render() {
-    return (
-      <div>
-        <footer className="footer">
-          <div>
-            <a className="text-muted" href="https://github.com/matiasraisanen/solidabis-koodihaaste-2022">Matias Räisänen 2022</a>
-          </div>
-        </footer>
-      </div>
-    );
-  }
+export default function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+        <div>
+          <a className="text-muted" href="https://github.com/matiasraisanen/solidabis-koodihaaste-2022">Matias Räisänen 2022</a>
+        </div>
+      </footer>
+    </div>
+  );
 }
-
-export default Footer;
-
