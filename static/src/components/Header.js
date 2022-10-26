d978e1e9e43298a89487b75a64c9451b06bf65bf
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-26 19:08:38 +0000

    add header

---
+++ static/src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <div>
+      <footer className="header">
+        <div>
+          FOOD FIGHT 2022
+        </div>
+      </footer>
+    </div>
+  );
+}
+
