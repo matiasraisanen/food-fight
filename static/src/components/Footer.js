69a7c885e31d21622adf45c7a1450fec973b2bea
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-28 12:02:36 +0000

    change footer text

--- static/src/components/Footer.js
+++ static/src/components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
     <div>
       <footer className="footer">
         <div>
-          <a className="text-muted" href="https://github.com/matiasraisanen/solidabis-koodihaaste-2022">Matias Räisänen 2022</a>
+          <a className="text-muted" href="https://github.com/matiasraisanen/solidabis-koodihaaste-2022">source @github</a>
         </div>
       </footer>
     </div>
