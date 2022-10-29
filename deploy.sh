9def66e8757e7b600e2e868bf1280f9b7e2e29d7
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-29 13:51:02 +0000

    update script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ G='\033[0;32m' # Green
 N='\033[0m'    # Neutral
 
 printf "${G}\n"
-printf "   ╓── MANAGEMENT SCRIPT FOR KOODIHAASTE APPLICATION\n"
+printf "   ╓── DEPLOY SCRIPT FOR KOODIHAASTE APPLICATION\n"
 printf "   ║\n"
 printf "   ╠═> 1. Run tests\n"
 printf "   ╠══> 2. Build static website\n"
@@ -16,15 +16,15 @@ printf "   ╚═══> 3. Deploy application to AWS\n"
 printf "${N}\n"
 
 
-# Run all the tests
-# (cd infra && npm run test)
+# Run all tests
+(cd infra && npm run test)
 
 # Build static website
-# (cd static && npm run build)
+(cd static && npm run build)
 
 # Deploy application to AWS
 
-# If you are not me, you should change this profile.
-# More @ https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+### Setup AWS profile name
 AWS_PROFILE_NAME="myPersonalAws"
-# (cd infra && npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all )
+
+(cd infra && npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all )
