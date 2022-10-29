416dbd4289baed253ba05254ca54959808f371cb
Matias Räisänen <matias.raisanen@gmail.com>
2022-10-29 13:38:14 +0000

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 # #!/bin/bash
-# # I like colours on my scripts :-)
 
-# R='\033[0;31m' # Red
-# Y='\033[0;33m' # Yellow
+# We want to bail on errors. If test fail, deployment will not be carried out.
+set -e
+
+
+# Some colours to make the print prettier
 G='\033[0;32m' # Green
 N='\033[0m'    # No Color
 
@@ -14,11 +16,11 @@ printf "\n"
 # More @ https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
 AWS_PROFILE_NAME="myPersonalAws"
 
+# Run all the tests
+(cd infra && npm run test)
+
 # Build static website
-cd static
-npm run build
-cd ..
+(cd static && npm run build)
 
 # Deploy application to AWS
-cd infra
-npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all 
+(cd infra && npx cdk deploy --profile $AWS_PROFILE_NAME --require-approval never --all )
